35a50415b8985577248e03c18d70720e40891326
Syed Kounain Abbas Rizvi <abbasmashaddy72@gmail.com>
2022-08-02 10:09:44 +0530

    Branch CES Completed

--- app/Http/Livewire/Form/BranchCES.php
+++ app/Http/Livewire/Form/BranchCES.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Livewire\Form;
 
+use App\Models\Branch;
+use App\Services\Helper;
 use Livewire\Component;
 
 class BranchCES extends Component
 {
+    // Model Values
+    public $house_no, $landmark, $locality_id, $pin_code, $registration_fee, $available_facilities, $manager_name, $manager_contact_no, $manager_email;
+
     // Custom Values
     public $data, $selectedLocalityId = null;
 
@@ -16,6 +21,62 @@ class BranchCES extends Component
     public function locality_changed($locality_id)
     {
         $this->selectedLocalityId = $locality_id;
+    }
+
+    protected $rules = [
+        'house_no' => '',
+        'landmark' => '',
+        'locality_id' => '',
+        'pin_code' => '',
+        'registration_fee' => '',
+        'available_facilities' => '',
+        'manager_name' => '',
+        'manager_contact_no' => '',
+        'manager_email' => '',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function store()
+    {
+        $validatedData = $this->validate();
+        $validatedData['locality_id'] = $this->selectedLocalityId['locality_id'];
+
+        Branch::create($validatedData);
+
+        notify()->success('Branch Saved Successfully!');
+
+        return $this->redirectRoute('branch.index');
+    }
+
+    public function update()
+    {
+        $validatedData = $this->validate();
+        $validatedData['locality_id'] = $this->selectedLocalityId['locality_id'] ?? $this->data->locality_id;
+
+        Branch::where('id', $this->data->id)->update($validatedData);
+
+        notify()->success('Branch Updated Successfully!');
+
+        return $this->redirectRoute('branch.index');
+    }
+
+    public function mount($data)
+    {
+        if (Helper::getRouteAction() != 'create') {
+            $this->house_no = $data->house_no;
+            $this->landmark = $data->landmark;
+            $this->selectedLocalityId = $data->locality_id;
+            $this->pin_code = $data->pin_code;
+            $this->registration_fee = $data->registration_fee;
+            $this->available_facilities = $data->available_facilities;
+            $this->manager_name = $data->manager_name;
+            $this->manager_contact_no = $data->manager_contact_no;
+            $this->manager_email = $data->manager_email;
+        }
     }
 
     public function render()
